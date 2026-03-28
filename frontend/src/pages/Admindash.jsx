c7437e30fb110dfba6e4e822f31773385138bbbd
deva shermale <shermaledeva@gmail.com>
2026-03-28 23:38:36 +0530

    final

--- frontend/src/pages/Admindash.jsx
+++ frontend/src/pages/Admindash.jsx
@@ -2,18 +2,50 @@ import React from 'react'
 import Adminform from '../components/Adminform'
 import Adminnav from '../components/Adminnav'
 function Admindash() {
+const [count, setCount] = useState({
+    total:0,
+    pending: 0,
+    approved: 0,
+    rejected:0,
+   
+  });
+  useEffect(()=>{
+    try {
+      const res = axios.get('http://localhost:8080/visitor/pass')
 
+      const visitor = res.data 
+
+    setCount ({
+      total: visitor.length,
+        pending: visitor.filter(v =>v.status === 'pending').length,
+        approved: visitor.filter(v =>v.status === 'Approved').length,
+         rejected:visitor.filter(v=>v.status=== 'rejected').length
+
+
+    })
+    } catch (error) {
+      
+    }
+  },[])
   return (
     <div>
 
-      <div>
+      <div className=' bg-amber-700'>
         <Adminnav />
         <div className=' grid grid-cols-3'>
-          <div className=' w-120 h-60  bg-blue-800  border-2 text-2xl text-center rounded m-2 '>whole employee</div>
-          <div className='w-120 h-60   bg-blue-800  border-2 text-2xl text-center rounded m-2 '>whole visitor</div>
-\          <div className='w-120 h-60  bg-blue-800    border-2 text-2xl text-center rounded m-2 '>visitor approved</div>
-          <div className='w-120 h-60   bg-blue-800  border-2 text-2xl text-center rounded m-2 '>rejected visitor</div>
-          <div className='w-120 h-60  bg-blue-800  border-2 text-2xl text-center rounded m-2 '>pending</div>
+          <div className='w-120 h-60   bg-blue-800  border-2 text-2xl text-center rounded m-2 '>whole visitor
+            <span className=' m-10  text-6xl flex items-center justify-center '> {count.total}</span>
+          </div>
+         
+        <div className='w-120 h-60  bg-blue-800    border-2 text-2xl text-center rounded m-2 '>visitor approved
+    <span className=' m-10  text-6xl flex items-center justify-center '>{count.approved}</span>
+</div>
+          <div className='w-120 h-60   bg-blue-800  border-2 text-2xl text-center rounded m-2 '>rejected visitor
+            <span className=' m-10  text-6xl flex items-center justify-center '>{count.rejected}</span>
+          </div>
+          <div className='w-120 h-60  bg-blue-800  border-2 text-2xl text-center rounded m-2 '>pending
+            <span className=' m-10  text-6xl flex items-center justify-center '>{count.pending}</span>
+          </div>
         </div>
       </div>
     </div>
--- frontend/src/pages/Empdash.jsx
+++ frontend/src/pages/Empdash.jsx
@@ -1,23 +1,54 @@
 import React from 'react'
 import Empnav from '../components/Empnav'
 function Empdash() {
+  const [count, setCount] = useState({
+    total:0,
+    pending: 0,
+    approved: 0,
+    rejected:0,
+   
+  });
+  useEffect(()=>{
+    try {
+      const res = axios.get('http://localhost:8080/visitor/pass')
+
+      const visitor = res.data 
+
+    setCount ({
+      total: visitor.length,
+        pending: visitor.filter(v =>v.status === 'pending').length,
+        approved: visitor.filter(v =>v.status === 'Approved').length,
+       
+         rejected:visitor.filter(v=>v.status=== 'rejected').length
+
+
+    })
+    } catch (error) {
+      
+    }
+  },[])
   return (
     <div>
-      <div className=' h-screen'>
+      <div className=' h-screen bg-amber-900'>
         <Empnav />
         <div className=' grid grid-cols-3 m-8'>
           <div className='w-120 h-60   bg-blue-800 border-2 text-2xl text-center rounded m-2'>
             whole visitors count
+            <span className=' m-10  text-6xl flex items-center justify-center '>{count.total}</span>
           </div>
         
           <div className='w-120 h-60  bg-blue-800 border-2 text-2xl text-center rounded m-2 '>
             approved visitor
+            <span className=' m-10  text-6xl flex items-center justify-center '>{count.approved}</span>
           </div>
           <div className=' w-120 h-60  bg-blue-800 border-2 text-2xl text-center rounded m-2'>
             Rejected visitor
+            <span className=' m-10  text-6xl flex items-center justify-center '>{count.rejected}</span>
           </div>
           <div className=' w-120 h-60   bg-blue-800 border-2 text-2xl text-center rounded m-2 '>
             pending visitor
+            <span className=' m-10  text-6xl flex items-center justify-center '> {count.pending}</span>
+           
           </div>
 
         </div>
